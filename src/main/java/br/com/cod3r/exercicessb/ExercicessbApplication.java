5b3c87d5db3b2ae3f8397323b498a10c71c1be94
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-19 10:46:07 -0500

    Initial commit

---
+++ src/main/java/br/com/cod3r/exercicessb/ExercicessbApplication.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exercicessb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicessbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicessbApplication.class, args);
+	}
+
+}
